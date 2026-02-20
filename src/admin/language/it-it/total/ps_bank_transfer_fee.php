96419bcf9934f62469de80070f71f83009eb7cfb
zsoltoroszlany82 <zsolt.oroszlany@playfulsparkle.com>
2026-02-20 10:10:52 +0100

    Translation normalization

--- src/admin/language/it-it/total/ps_bank_transfer_fee.php
+++ src/admin/language/it-it/total/ps_bank_transfer_fee.php
@@ -5,22 +5,22 @@ $_['heading_contact']                      = 'Contatta il Supporto';
 
 // Text
 $_['text_extension']                       = 'Estensioni';
-$_['text_success']                         = 'Successo: Hai modificato la Commissione Bonifico Bancario!';
-$_['text_edit']                            = 'Modifica Commissione Bonifico Bancario';
+$_['text_success']                         = 'Successo: Hai modificato la commissione per il bonifico bancario!';
+$_['text_edit']                            = 'Modifica la Commissione per Bonifico Bancario';
 $_['text_contact']                         = '<p>Per ulteriore assistenza, contatta il nostro team di supporto:</p><ul><li><strong>Contatto:</strong> <a href="mailto:%s">%s</a></li><li><strong>Documentazione:</strong> <a href="%s" target="_blank" rel="noopener noreferrer">Documentazione Utente</a></li></ul>';
 
 // Tab
 $_['tab_general']                          = 'Generale';
-$_['tab_help_and_support']                 = 'Aiuto &amp; Supporto';
+$_['tab_help_and_support']                 = 'Assistenza e Supporto';
 
 // Entry
 $_['entry_fee']                            = 'Commissione';
 $_['entry_tax_class']                      = 'Classe Fiscale';
 $_['entry_status']                         = 'Stato';
-$_['entry_sort_order']                     = 'Ordine di Visualizzazione';
+$_['entry_sort_order']                     = 'Ordinamento';
 
 // Error
 $_['error_total_ps_bank_transfer_fee_fee'] = 'La commissione non può essere vuota, inferiore o uguale a zero';
 
 // Error
-$_['error_permission']                     = 'Avviso: Non hai i permessi per modificare la Commissione Bonifico Bancario!';
+$_['error_permission']                     = 'Attenzione: Non hai il permesso di modificare la commissione per il bonifico bancario!';
